d407a4349f9f9f83a0bf0f7539f3bde82d7bb2dc
Brau <Judson.Brau@d11.org>
2019-08-28 19:29:54 -0600

    riddle commit

--- riddle.py
+++ riddle.py
@@ -3,12 +3,27 @@
 # riddle.py
 import time
 
-riddles = {
-    "1": ["Whats red and greed and goes 100mph?", "a frog in a blender"],
-    "Knock knock!": "Control Freak who?",
-    "3": ["What do you call cheese that isn't yours?", "nacho cheese"]
+riddles = [
+    ["Whats red and greed and goes 100mph?", ["a frog in a blender", "Correct!"], "Close! A frog in a blender"],
+    ["Knock knock!", None, "Control Freak", None, "Ok, now your supposed to say \"Control Freak who?\" okay?"],
+    ["What do you call cheese that isn't yours?", ["nacho cheese", "Correct!"], "Almost!, its nacho cheese"]
+]
 
-}
+def printRiddle(riddles):
+    rididx = 0
+    print(riddles[0])
+    for string, i in enumerate(riddles[rididx]):
+        print(i)
+        if type(string) == list:
+            answer = input()
+            if answer == string[0] or (not string[0]):
+                print(string[1])
+            else:
+                print(string[i+1])
+        else:
+            print(string)
+    rididx += 1
+
 
 try:
     num = input("What riddle do you want to hear, 1, 2, or 3? ")
@@ -16,17 +31,18 @@ except:
     print("Please enter in a number next time")
     quit()
 
-if int(num) == 2:
-    print("Knock knock!")
-    input()
-    print("Control Freak.")
-    time.sleep(1.7)
-    print("Okay, now your supposed to say 'Control Freak who?'")
-else:
-    set_up = riddles[num]
-    print(set_up[0])
-    answer = input(": ")
-    if answer.lower() == set_up[1].lower():
-        print("Correct!")
-    else:
-        print("Close!", set_up[1])
+printRiddle(riddles)
+# if int(num) == 2:
+#     print("Knock knock!")
+#     input()
+#     print("Control Freak.")
+#     time.sleep(1.7)
+#     print("Okay, now your supposed to say 'Control Freak who?'")
+# else:
+#     set_up = riddles[num]
+#     print(set_up[0])
+#     answer = input(": ")
+#     if answer.lower() == set_up[1].lower():
+#         print("Correct!")
+#     else:
+#         print("Close!", set_up[1])
